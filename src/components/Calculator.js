f3746b3b81ce3e328e434599cc11e209d2d96844
Fondem Junior <fondemjunior@yahoo.com>
2021-11-25 01:22:55 +0100

    Implement event handlers, practice stat and props and test the app

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import './Calculator.css';
+// import operate from '../logic/operate';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: {
+        total: '0',
+        next: '',
+        operation: '',
+      },
+    };
+    this.onKeyPressed = this.onKeyPressed.bind(this);
+  }
+
+  onKeyPressed = (obj, buttonName) => {
+    const newObj = calculate(obj, buttonName);
+    if (!newObj.next && !newObj.total) {
+      newObj.next = 0;
+    }
+    this.setState({ obj: newObj });
+  }
+
   render() {
+    const { obj } = this.state;
     return (
+
       <div className="container">
+        <p className="calc-screen">
+          {obj.operation}
+          {obj.next}
+        </p>
         <form>
-          <input type="text" placeholder="0" />
+          <input type="text" placeholder="0" value={obj.total} />
         </form>
         <div className="keypad">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="symbol">÷</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="symbol">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="symbol">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="symbol">+</button>
-          <button type="button" id="zero">0</button>
-          <button type="button">.</button>
-          <button type="button" className="symbol">=</button>
+          <button onClick={() => this.onKeyPressed(obj, 'AC')} type="button" buttonName="AC">AC</button>
+          <button onClick={() => this.onKeyPressed(obj, '+/-')} type="button" buttonName="+/-">+/-</button>
+          <button onClick={() => this.onKeyPressed(obj, '%')} type="button" buttonName="%">%</button>
+          <button onClick={() => this.onKeyPressed(obj, '÷')} type="button" className="symbol" buttonName="÷">÷</button>
+          <button onClick={() => this.onKeyPressed(obj, '7')} type="button" buttonName="7">7</button>
+          <button onClick={() => this.onKeyPressed(obj, '8')} type="button" buttonName="8">8</button>
+          <button onClick={() => this.onKeyPressed(obj, '9')} type="button" buttonName="9">9</button>
+          <button onClick={() => this.onKeyPressed(obj, 'x')} type="button" className="symbol" buttonName="x">x</button>
+          <button onClick={() => this.onKeyPressed(obj, '4')} type="button" buttonName="4">4</button>
+          <button onClick={() => this.onKeyPressed(obj, '5')} type="button" buttonName="5">5</button>
+          <button onClick={() => this.onKeyPressed(obj, '6')} type="button" buttonName="6">6</button>
+          <button onClick={() => this.onKeyPressed(obj, '-')} type="button" className="symbol" buttonName="-">-</button>
+          <button onClick={() => this.onKeyPressed(obj, '1')} type="button" buttonName="1">1</button>
+          <button onClick={() => this.onKeyPressed(obj, '2')} type="button" buttonName="2">2</button>
+          <button onClick={() => this.onKeyPressed(obj, '3')} type="button" buttonName="3">3</button>
+          <button onClick={() => this.onKeyPressed(obj, '+')} type="button" className="symbol" buttonName="+">+</button>
+          <button onClick={() => this.onKeyPressed(obj, '0')} type="button" id="zero" buttonName="0">0</button>
+          <button onClick={() => this.onKeyPressed(obj, '.')} type="button" buttonName=".">.</button>
+          <button onClick={() => this.onKeyPressed(obj, '=')} type="button" className="symbol" buttonName="=">=</button>
         </div>
       </div>
     );
