cd41a333521ccd0799b33b1692f18d78f1e40596
Fondem Junior <fondemjunior@yahoo.com>
2021-12-02 23:43:56 +0100

    Create home, Calculator and Quote pages

---
+++ src/pages/Cal.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const Cal = () => {
+  return (
+    <div>
+      <div>
+      <p>Lets do some math!</p>
+    </div>
+    <div>
+      <Calculator />
+    </div>
+    </div>
+  );
+};
+
+export default Cal;
